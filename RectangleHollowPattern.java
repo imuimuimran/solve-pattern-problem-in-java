1c5c0f0569c3058bd621f27a0e98d66ad1cd283f
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-10 15:52:27 +0600

    add a file where a rectangle hollow parrern is solved.

---
+++ RectangleHollowPattern.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;  
+public class RectangleHollowPattern  {  
+    public static void main(String[] args)  {  
+        Scanner scanner = new Scanner(System.in);  
+        System.out.print("Enter the number of row: ");  
+        int row = scanner.nextInt();
+        System.out.print("Enter the number of column: ");  
+        int column = scanner.nextInt();     
+        
+        /*
+        The pattern looks like the below:
+        *****
+        *   *
+        *   *
+        ***** 
+        */ 
+        
+        for(int i = 1; i <= row; i++) {
+            for(int j = 1; j <= column; j++) {
+                if(i == 1 || j == 1 || i == row || j == column) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }  
+}  

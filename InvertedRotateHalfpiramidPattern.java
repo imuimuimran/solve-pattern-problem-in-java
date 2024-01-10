ae84a4dff950d56aaefa913d3d0b0fd29338d3ef
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-11 00:46:13 +0600

    Add Inverted rotate Half piramid pattern.

---
+++ InvertedRotateHalfpiramidPattern.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;  
+public class InvertedRotateHalfpiramidPattern {  
+    public static void main(String[] args)  {  
+        Scanner scanner = new Scanner(System.in);  
+        System.out.print("Enter the number of row: ");  
+        int row = scanner.nextInt();
+        
+        /*
+        The pattern looks like the below:
+            *
+           **
+          ***
+         ****
+        *****
+        */ 
+        
+        for(int i = 1; i <= row; i++) {
+            for(int j = 1; j <= row-i; j++) {
+                    System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }  
+}  

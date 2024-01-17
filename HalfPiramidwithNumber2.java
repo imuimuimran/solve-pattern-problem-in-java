86dae309305af4a5ee5a084b2feb0875efd803ef
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-18 00:29:41 +0600

    add Half Piramid with Number Pattern.

---
+++ HalfPiramidwithNumber2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;  
+public class HalfPiramidwithNumber2  {  
+    public static void main(String[] args)  {  
+        Scanner scanner = new Scanner(System.in);  
+        System.out.print("Enter the number of row: ");  
+        int row = scanner.nextInt();
+        
+        /*
+        The pattern looks like the below:
+            1
+           1 2
+          1 2 3
+         1 2 3 4
+        1 2 3 4 5
+        */ 
+        
+        for(int i = 1; i <= row; i++) {
+            for(int j = 1; j <= row - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }  
+}  

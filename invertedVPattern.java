91c46cae97479c337bbf3fd14bf1560ca85c8688
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-28 20:36:43 +0600

    This is inverted V pattern template.

---
+++ invertedVPattern.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;  
+public class invertedVPattern {  
+    public static void main(String[] args)  {  
+        Scanner scanner = new Scanner(System.in);  
+        System.out.print("Enter the number of row: ");  
+        int row = scanner.nextInt();
+        
+        /*
+        The pattern looks like the below:
+
+           *
+         *  *
+        *    *
+
+        */ 
+        
+        int i, j;
+
+        
+        // Inverted V pattern
+        for (i = 1; i <= row; i++) {
+            for (j = i; j < row; j++) {
+                System.out.print(" ");//Print spaces
+            }
+
+            for (j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1))//Logic for printing star
+                    System.out.print("*");
+                else
+                    System.out.print(" ");//if logic fails print space
+            }
+
+            System.out.println("");
+        }
+
+        scanner.close();
+    }  
+}  
